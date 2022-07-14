08a0cce165e7345dfdba5fce606acd81379c2fd1
Aruming <0430kar@naver.com>
2022-07-14 16:57:34 +0900

    파일 입출력 기능 구현

--- src/test/java/com/ssg/exam/AppTest.java
+++ src/test/java/com/ssg/exam/AppTest.java
@@ -24,12 +24,12 @@ public class AppTest {
     }
 
     @Test
-    public void 출력을_모니터에_하지_않고_문자열로_얻기() {
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-        System.out.print("안녕");
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+    public void 문자열을_파일에_저장() {
+        Util.file.mkdir("test_data");
+        Util.file.saveToFile("test_data/1.txt", "안녕");
 
-        assertEquals("안녕", rs);
+        String body = Util.file.readFromFile("test_data/1.txt");
+
+        assertEquals("안녕", body);
     }
 }
