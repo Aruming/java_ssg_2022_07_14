45732034f186ac06add224902312ce110cabbbf8
Aruming <0430kar@naver.com>
2022-07-15 14:33:38 +0900

    목록 기능 추가, WiseSaying 클래스 도입

--- src/main/java/com/ssg/exam/App.java
+++ src/main/java/com/ssg/exam/App.java
@@ -1,10 +1,13 @@
 package com.ssg.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private Scanner sc;
     int wiseSayingLastId;
+    List<WiseSaying> wiseSayings = new ArrayList<>();
 
     App(Scanner sc){
         this.sc = sc;
@@ -26,12 +29,26 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
                     int id = ++wiseSayingLastId;
+
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    wiseSayings.add(wiseSaying);
                     System.out.printf("%d번 명언이 등록되었습니다.", id);
 
                     break;
 
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying2 = wiseSayings.get(i);
+
+                        System.out.printf("%d / %s / %s\n", wiseSaying2.id, wiseSaying2.author, wiseSaying2.content);
+                    }
+                    break;
+
                 case "종료":
-                break outer;
+                    break outer;
             }
         }
     }
---
+++ src/main/java/com/ssg/exam/WiseSaying.java
@@ -0,0 +1,13 @@
+package com.ssg.exam;
+
+public class WiseSaying {
+    int id;
+    String content;
+    String author;
+
+    WiseSaying(int id, String content, String author){
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+}
